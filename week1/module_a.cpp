79c72ead100b71f2a17715287e742c50fb8e8393
Masood Khan Patel <patel.masoodkhan@gmail.com>
2024-02-21 09:03:10 -0500

    more examples

--- week1/module_a.cpp
+++ week1/module_a.cpp
@@ -9,3 +9,14 @@
 //     std::cout << "Module A: share_me at " << &share_me   << '\n';
 //     std::cout << "Module A: share_me is " <<  share_me++ << '\n';
 // }
+
+
+Animal* a = new Animal();
+Box<Animal> animalBox = new Box<Animal>(a);
+
+// Box -> content
+
+
+
+
+std::cout << "Animal sound from box: " << animalBox.getContent().sound() << std::endl;
--- week1/sizeof.cpp
+++ week1/sizeof.cpp
@@ -1,18 +1,18 @@
-// sizeof Operator
-// sizeofArray.cpp
+// // sizeof Operator
+// // sizeofArray.cpp
 
-#include <iostream>
+// #include <iostream>
 
-int foo(double a[])
-{
-    a[0] = 1.0;
-    return sizeof a;
-}
+// int foo(double a[])
+// {
+//     a[0] = 1.0;
+//     return sizeof a;
+// }
 
-int main()
-{
-    double a[] = {1.1, 2.2, 3.3, 4.4};
+// int main()
+// {
+//     double a[] = {1.1, 2.2, 3.3, 4.4};
 
-    std::cout << sizeof a << std::endl;
-    std::cout << foo(a) << std::endl;
-}
+//     std::cout << sizeof a << std::endl;
+//     std::cout << foo(a) << std::endl;
+// }
